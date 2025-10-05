65532abcd601efd60c6edebcdde37c327fe9a89a
yashaswi17 <110465185+yashaswi17@users.noreply.github.com>
2025-10-05 13:25:11 +0530

    first commit

---
+++ new.py
@@ -0,0 +1,29 @@
+from langchain_core.prompts import ChatPromptTemplate 
+from langchain_core.output_parsers import StrOutputParser
+from langchain_ollama import OllamaLLM
+
+import streamlit as st 
+import os
+
+'''Prompt Template'''
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "you are an expert in data science and a helpful assistant."
+        ),
+        ("user", "Question : {question}"),
+    ]
+)
+
+"Streamlit Framework"
+st.title("langchain project")
+input_text = st.text_input("search the topic you want ")
+
+"ollama model"
+llm = OllamaLLM(model="llama3.2:1b")
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    st.write(chain.invoke({"question": input_text}))
